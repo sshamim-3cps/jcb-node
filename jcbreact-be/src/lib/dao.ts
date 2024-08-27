a403f8b1e60d4f644e24c67e7d3437c362332dfc
sshamim-3cps <sshamim@3clogic.com>
2024-08-27 20:28:53 +0530

    fixed dir name in workflow yaml

--- jcbreact-be/src/lib/dao.ts
+++ jcbreact-be/src/lib/dao.ts
@@ -9,7 +9,7 @@ export async function getConversationById(conversationId: number) {
     });
 }
 
-//to trigger workflow acion
+//to trigger workflow acion [2]
 export async function addMessagesToConversation(conversationId: number, messages: Partial<Message>[]) {
     try {
         console.log('Adding messages to conversation#id:', conversationId);
